f3c0f5c5249ea7eccbb89e75cd5cb46b975b30d9
edro23 <ethandrover@hotmail.com>
2024-09-27 16:04:19 -0230

    Sept27th-PostCommandTest

--- myApp/index.js
+++ myApp/index.js
@@ -1,11 +1,45 @@
+//1
+const Course = require(`./models/schedule.model.js`);
+
 const express = require('express')
 const app = express()
 const port = 3000
+
+//2
+app.use(express.json());
+
+//3
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.post('/api/courses', async (req, res) => {
+  // The browser uses the GET mothod to send any message, so 
+  // use postman to send a POST message to the app.
+  try {
+      // Here we'll use our model to save the data.
+  // we expect req.body will contain a course record to save to the db.
+      const sched = await Course.create(req.body);
+      res.status(200).json(sched);
+  }
+  catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+});
+
+//app.listen(port, () => {
+  //console.log(`Example app listening on port ${port}`)
+//})
+
+mongoose.connect('mongodb://Student12:Password1@logan', { dbName: 'home12' })
+    .then(() => {
+        console.log("Connected to the database!");
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
+    })
+    .catch(() => {
+        console.log("Failed to connect to the database.");
+    });
---
+++ myApp/models/schedule.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const SessionsSubSchema = mongoose.Schema ({
+    day: {
+        type: Number,
+        required: true,
+        defaukt:1
+    },
+    startTime: {
+        type: Number,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    room: {
+        type: String,
+        required: true
+    }
+});
+const ScheduleSchema = mongoose.Schema ({
+    courseName: {
+        type: String,
+        required: [true,"please enter the course name"],
+    },
+    courseTitle: {
+        type: String,
+        required: [true, "please enter the course title"],
+    },
+    sessions: {
+        type:[SessionsSubSchema]
+    },
+},
+{
+    timestamps: true
+});
+
+const Course = mongoose.model("Course",ScheduleSchema);
+
+module.exports = Course;
+
